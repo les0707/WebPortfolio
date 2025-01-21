116f464726281cefc91778e96f0ed2a70696156c
Leigh <esclanda97@gmail.com>
2025-01-21 16:40:58 +0800

    changes in the web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AboutController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('about');
 });
 
 Route::get('/about', [AboutController::class, 'index'])->name('about');
